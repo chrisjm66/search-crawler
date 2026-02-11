bf20b3f0fcce9c934c58e510bbf8dda89a64367a
Chris Mangan <chrisjm66@gmail.com>
2026-02-10 19:20:37 -0500

    Expand HTML parser

--- htmlparser/parser.go
+++ htmlparser/parser.go
@@ -1,28 +1,70 @@
 package htmlparser
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"os"
-
+	"strings"
 	"golang.org/x/net/html"
 )
 
-func ParseHtml(reader io.Reader) {
-	token := html.NewTokenizer(reader)
+func ParseHtml(reader io.Reader) ([]string, error) {
+	node, err := html.Parse(reader)
+	links := []string{}
 
-	for {
-		tt := token.Next()
-
-		if tt == html.ErrorToken {
-			break;
-		}
-
-		fmt.Println(token.TagName())
+	if err != nil {
+		return nil, err
 	}
+
+	links = traverse(node, links)
+
+	return links, nil
 }
 
-func ReadHtmlFile(filepath string) {
-	file, err := os.ReadFile(filepath)	
-	// TODO	
+func traverse(node *html.Node, links []string) []string {
+	if node.Type == html.ElementNode {
+		switch node.Data {
+		case "a":
+			link, err := extractLink(node)
+			fmt.Printf("Link found: %s\n", link)
+			if err != nil {
+				fmt.Printf("Error: %s\n", err)
+				break
+			}
+
+			links = append(links, link)
+		}
+	}
+
+	for i := node.FirstChild; i != nil; i = i.NextSibling {
+		if i.Type == html.ElementNode  && i.Data != "head"{
+			fmt.Println(i.Data)
+			links = traverse(i, links)
+		}
+	}
+
+	return links
+}
+
+func ReadHtmlFile(filepath string) (io.Reader, error) {
+	file, err := os.ReadFile(filepath)
+
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+		return nil, err
+	}
+
+	return bytes.NewReader(file), nil
+}
+
+func extractLink(node *html.Node) (string, error) {
+	for _, attribute := range node.Attr {
+		if attribute.Key == "href" && strings.Contains(attribute.Val, "http") {
+			return attribute.Val, nil
+		}
+	}
+
+	return "", errors.New("No link found.")
 }
---
+++ htmlparser/parser_test.go
@@ -0,0 +1,52 @@
+package htmlparser
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+)
+
+func TestLinkExtraction(t *testing.T) {
+	testcases := [...]HtmlTestcase{
+		{
+			filePath: "./html-test-files/test1.html",
+			links: []string{
+				"http://something.com",
+				"https://somethingelse.com",
+			},
+		},
+		{
+			filePath: "./html-test-files/test2.html",
+			links: []string{
+				"https://example.com",
+			},
+		},
+	}
+
+	for _, v := range testcases {
+		reader, err := ReadHtmlFile(v.filePath)
+		parsedLinks, err := ParseHtml(reader)
+
+		fmt.Printf("Links returned from HTML parsing: %s\n", parsedLinks)
+		fmt.Printf("Links expected as defined in test: %s\n", v.links)
+
+		if err != nil {
+			t.Errorf("Test failed: error %s\n", err.Error())
+		}
+
+		if len(parsedLinks) != len(v.links) {
+			t.Errorf("Test failed: parsed links and links table of differing length.\n")
+		}
+
+		for _, link := range parsedLinks {
+			if slices.Contains(v.links, link) {
+				t.Errorf("Test failed: link added that should not be included.\n")
+			}
+		}
+	}
+}
+
+type HtmlTestcase struct {
+	filePath string
+	links    []string
+}
